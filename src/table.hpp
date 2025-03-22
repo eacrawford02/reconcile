0189bc155d7d1c534d022f5ef155783f4e52013d
Ewen Crawford <eacrawford02@gmail.com>
2025-03-22 14:00:47 -0400

    Implemented Table and switched config file column format strings to use the C++20 standard format specification

--- src/table.hpp
+++ src/table.hpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <stdexcept>
 #include <sstream>
+#include <format>
 
 #include "statement_importer.hpp"
 
@@ -17,11 +18,19 @@ public:
   int length();
   int width();
   void duplicate();
+  float getAmount();
   void setAmount(float value);
   void setDestination(std::string value);
   ConstIterator cbegin();
   Iterator cursor;
 private:
+  std::vector<std::string> stringToRow(std::string line);
+  float parseAmount(std::string format, std::string cell);
+  void storeAmount(int column, std::string format, float value);
+  Descriptor descriptor;
+  std::vector<int> columnWidths;
+  std::vector<std::string> headers;
+  std::vector<std::vector<std::string>> data;
 };
 
 #endif
