aef75260fecc50aed840eda3e8419ef1ba0eb29e
Ewen Crawford <eacrawford02@gmail.com>
2025-04-06 17:26:37 -0400

    Implemented prompting functionality

--- src/prompt.hpp
+++ src/prompt.hpp
@@ -1,6 +1,7 @@
 #ifndef PROMPT_H
 #define PROMPT_H
 
+#include <vector>
 #include <string>
 
 #include <ncurses.h>
@@ -8,9 +9,13 @@
 class Prompt {
 public:
   Prompt(WINDOW* window);
+  void debitPrompt(std::vector<std::string> row);
+  void creditPrompt(std::vector<std::string> row);
   Prompt& operator<<(char c);
   void clear();
 private:
+  WINDOW* window;
+  void draw(std::vector<std::string> row, std::string message);
 };
 
 #endif
