e875e3b936175a99de91e00c6e669ebb2476b8ba
Ewen Crawford <eacrawford02@gmail.com>
2025-04-12 20:34:02 -0400

    Updated Input class to reflect changes to prompting scheme

--- src/input.cpp
+++ src/input.cpp
@@ -1,75 +1,27 @@
 #include "input.hpp"
 
-Input::Input(TableViewArray& tableViewArray, Prompt prompt) :
+Input::Input(TableViewArray& tableViewArray, Prompt& prompt) :
     tableViewArray{tableViewArray}, prompt{prompt} {
   // Set up initial prompt
   Table& table = tableViewArray.focusedTable();
   if (table.getAmount() >= 0) {
-    this->prompt.debitPrompt(table.displayRow(table.cursor - table.cbegin()));
+    prompt.debitPrompt(table.displayRow(table.cursor - table.cbegin()));
   } else {
-    this->prompt.creditPrompt(table.displayRow(table.cursor - table.cbegin()));
+    prompt.creditPrompt(table.displayRow(table.cursor - table.cbegin()));
   }
-
-  cursor = buffer.cbegin();
 }
 
-void Input::process() {
+void Input::evaluate() {
   while (!quit) {
-    wchar_t input = getch();
-    state = nextState(input);
+    std::string input = prompt.getInput();
 
-    switch (input) {
-      case KEY_ENTER:
-      case '\n':
-      case '\r':
-	evaluateInput(buffer);
-	break;
-      case KEY_BACKSPACE:
-      case '\b':
-      case 127:
-	if (cursor != buffer.cbegin()) {
-	  cursor--;
-	  buffer.erase(cursor);
-	}
-	prompt.printInput(buffer, cursor - buffer.cbegin());
-	break;
-      case KEY_LEFT:
-	if (cursor != buffer.cbegin()) cursor--;
-	prompt.printInput(buffer, cursor - buffer.cbegin());
-	break;
-      case KEY_RIGHT:
-	if (cursor != buffer.cend()) cursor++;
-	prompt.printInput(buffer, cursor - buffer.cbegin());
-	break;
-      default:
-	buffer.insert(cursor, input);
-	cursor++;
-	prompt.printInput(buffer, cursor - buffer.cbegin());
-	break;
+    if (input == "q") {
+      quit = true;
+    } else if (input == "s") {
+      // TODO: scroll down
+    } else if (input == "b") {
+      // TODO: scroll up
+    } else {
     }
-  }
-}
-
-Input::State Input::nextState(wchar_t input) {
-  State next;
-
-  switch (state) {
-    case INSERT:
-      break;
-    case BACKSPACE:
-      break;
-    case QUIT:
-      break;
-  }
-  
-  return next;
-}
-
-void Input::evaluateInput(std::string s) {
-  if (s == "q") {
-    quit = true;
-  } else if (s == "s") {
-  } else if (s == "b") {
-  } else {
   }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ int main(int argc, char* argv[]) {
   Prompt prompt{promptContent};
   Input input{tableViewArray, prompt};
 
-  input.process();
+  input.evaluate();
 
   delwin(tableContent);
   delwin(promptBorder);
