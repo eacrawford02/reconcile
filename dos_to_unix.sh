e7db390b39beb40454e749d4024fc7d029186740
Ewen Crawford <eacrawford02@gmail.com>
2026-04-01 19:36:25 -0400

    Updated dos_to_unix.sh script to print usage message.

--- dos_to_unix.sh
+++ dos_to_unix.sh
@@ -1,6 +1,22 @@
 #!/usr/local/bin/bash
 
-for file in *.csv; do
+if [ -z "$1" ]; then
+  echo "Usage: ./dos_to_unix.sh file [file ...]"
+  exit 1
+fi
+
+for arg in "$@"; do
+  case $arg in
+    -h*)
+      ;&
+    --help*)
+      echo "Usage: ./dos_to_unix.sh file [file ...]"
+      exit 1
+      ;;
+  esac
+done
+
+for file in "$@"; do
   if [[ -f "$file" ]]; then
     sed 's///g' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
   fi
